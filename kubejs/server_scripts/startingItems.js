b5f6b52b4351dfd0753d749ca09f7e3296c70f56
muqsit <abdemuqsit@hotmail.com>
2021-11-10 11:05:47 +0500

    Updated to 0.3.8a

--- kubejs/server_scripts/startingItems.js
+++ kubejs/server_scripts/startingItems.js
@@ -2,9 +2,13 @@
 
 //giving the player an Akashic tome on their first login
 events.listen('player.logged_in', function (event) {
+  event.server.schedule(10, event.server, function (callback) {
     if (!event.hasGameStage('starting_items')) {
-    event.addGameStage('starting_items')
-    event.player.give(Item.of('akashictome:tome', AKASHIC_TOME_NBT));
-    event.player.give('ftbquests:book');
-  }  
+      event.player.getInventory().clear();
+      event.addGameStage('starting_items')
+      event.player.give(Item.of('akashictome:tome', AKASHIC_TOME_NBT));
+      event.player.give('ftbquests:book');
+    }  
+  })
+ 
 })
